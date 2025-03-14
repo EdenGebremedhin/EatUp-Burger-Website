ea0360d2911c91cf3da9649e47e571f4afd1a185
EdenGebremedhin <edhabtetsion@gmail.com>
2025-03-14 22:47:45 +0300

    updates

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,18 +3,19 @@ import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem';
 
+
 const FoodDisplay = ({category}) => {
 
     const {food_list }= useContext(StoreContext);
     
   return (
-    <div className='food-displaay' id='food-display'>
+    <div className='food-display' id='food-display'>
         <h2>Top dishes near you</h2>
         <div className='food-display-list'>
-            {food_list.map((item, index)=>)}
-            return <FoodItem key={index} 
+            {food_list.map((item, index)=>{
+             return <FoodItem key={index} 
                 id={item._id} name={item.name} description={item.description} 
-                    price={item.price} image={item.image}/>
+                    price={item.price} image={item.image}/>})}
         </div>
     </div>
   )
