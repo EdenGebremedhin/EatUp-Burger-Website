0bc40cf1dc2fb26ebc6797951234f3556ab6699e
EdenGebremedhin <edhabtetsion@gmail.com>
2025-03-15 13:34:52 +0300

    finalizing home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
-
+import Footer from "./components/Footer/Footer.jsx";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 
 const App = () => {
   return (
+    <>
     <div className="app">
       <Navbar />
       {/* <Router> */}
@@ -18,6 +19,8 @@ const App = () => {
         </Routes>
       {/* </Router> */}
     </div>
+    <Footer />
+    </> 
   );
 };
 
