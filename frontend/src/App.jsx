915bf958d7b3e4f09aa7622d6465782865ffebcf
EdenGebremedhin <edhabtetsion@gmail.com>
2025-03-27 20:45:45 +0300

    responsive update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer.jsx";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 const App = () => {
+
+const [showLogin, setShowLogin] = useState(false)
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className="app">
-      <Navbar />
+      <Navbar setShowLogin = {setShowLogin}/>
       {/* <Router> */}
         <Routes>
           <Route path="/" element={<Home />} />
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,32 +1,55 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets.js'
-import {Link} from 'react-router-dom' 
+import React, { useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets.js";
+import { Link } from "react-router-dom";
 
+const Navbar = ({setShowLogin}) => {
 
-const Navbar = () => {
+  const [menu, setMenu] = useState("Menu");
 
-    const [menu, setMenu] = useState("Menu");
- 
   return (
-    <div className='navbar'>
-        <img src={assets.logo} alt="" className="logo" />
-        <ul className="navbar-menu">
-            <Link to='/' onClick={()=> setMenu("Home")} className={menu==="Home"?"active":""}>Home</Link>
-            <a href='#explore-menu' onClick={()=> setMenu("Contact-us")} className={menu==="Contact-us"?"active":""}>Menu</a>
-            <a href='#app-download' onClick={()=> setMenu("Menu")} className={menu==="Menu"?"active":""}>Mobile App</a>
-            <a href='#footer' onClick={()=> setMenu("Mobile-app")} className={menu==="Mobile-app"?"active":""}>Contact Us</a>
-        </ul>
-        <div className="navbar-right">
-            <img src= {assets.search_icon} alt="" />
-            <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
-                <div className="dot"></div>
-            </div>
-            <button>sign in</button>
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <Link
+          to="/"
+          onClick={() => setMenu("Home")}
+          className={menu === "Home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("Contact-us")}
+          className={menu === "Contact-us" ? "active" : ""}
+        >
+          Menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("Menu")}
+          className={menu === "Menu" ? "active" : ""}
+        >
+          Mobile App
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("Mobile-app")}
+          className={menu === "Mobile-app" ? "active" : ""}
+        >
+          Contact Us
+        </a>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
         </div>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
