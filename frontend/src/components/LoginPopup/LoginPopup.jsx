8f9079bf908c5bb1e902f94693179ac26a4fe302
EdenGebremedhin <edhabtetsion@gmail.com>
2025-03-28 00:41:41 +0300

    popup update

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets'
 
 const LoginPopup = ({setShowLogin}) => {
 
-    const [currentState, setCurrentState] = useState("Sign Up")
+    const [currentState, setCurrentState] = useState("Login")
 
   return (
     <div className='login-popup'>
@@ -13,6 +13,22 @@ const LoginPopup = ({setShowLogin}) => {
                 <h2>{currentState}</h2>
                 <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div>
+            <div className="login-popup-input">
+                {currentState === "Login"?<></>:
+                <input type="text" placeholder='Your name' required />}
+                <input type="email" placeholder='Your email' required/>
+                <input type="password" placeholder='Your password' required />
+            </div>
+            <button>{currentState==="Sign Up"?"Create account":"Login"}</button>
+            <div className="login-popup-condition">
+                <input type="checkbox" required/>
+                <p>By continuing, i agree to the terms of use & privacy policy</p>
+            </div>
+
+            {currentState === "Login"
+                ?<p>Create a new account? <span onClick={()=>setCurrentState("Sign Up")}>Click here</span></p>
+                :<p>Already have an account? <span onClick={()=>setCurrentState("Login")}>Login here</span></p>
+            }
         </form>
     </div>
   )
