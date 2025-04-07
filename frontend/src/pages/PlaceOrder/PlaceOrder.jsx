aef4e968a1abcebed73d86e3d93a0e6b5b9bfcaf
EdenGebremedhin <edhabtetsion@gmail.com>
2025-04-07 23:12:48 +0300

    order page update

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -8,27 +8,27 @@ const PlaceOrder = () => {
 
   return (
     <form action="" className="place-order">
-      <div className="plce-order-left">
+      <div className="place-order-left">
         <p className="title">Delivery Information</p>
-        <div className="multi-fields">
+        <div className="multi-field">
           <input type="text" placeholder='First name' />
           <input type="text" placeholder='Last name'/>
         </div>
         <input type="email" placeholder='Email address'/>
         <input type="text" placeholder='Street' />
-        <div className="multi-fields">
+        <div className="multi-field">
           <input type="text" placeholder='City' />
           <input type="text" placeholder='State'/>
         </div>
 
-        <div className="multi-fields">
+        <div className="multi-field">
           <input type="text" placeholder='Zip code' />
           <input type="text" placeholder='Country'/>
         </div>
         <input type="text" placeholder='Phone'/>
 
       </div>
-      <div className="plce-order-right">
+      <div className="place-order-right">
       <div className="cart-total">
           <h2>Cart Totals</h2>
 
